e50ff3f61b5f72919997f80b484f396892824790
hab <mohab.khalid.mahmoud@gmail.com>
2025-09-15 22:22:18 +0300

    Initial Commit

---
+++ ApexPredators/ApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApexPredatorsApp.swift
+//  ApexPredators
+//
+//  Created by Mohab Khalid on 15/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct ApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ApexPredatorsTests/ApexPredatorsTests.swift
@@ -0,0 +1,17 @@
+//
+//  ApexPredatorsTests.swift
+//  ApexPredatorsTests
+//
+//  Created by Mohab Khalid on 15/09/2025.
+//
+
+import Testing
+@testable import ApexPredators
+
+struct ApexPredatorsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
